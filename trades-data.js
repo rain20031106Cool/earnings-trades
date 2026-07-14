e2274e1d15937aec1e7e7131e015a3c8e96d7722
rain20031106Cool <rain20031106@gmail.com>
2026-07-14 22:49:41 +0800

    Add files via upload

---
+++ trades-data.js
@@ -0,0 +1,74 @@
+const trades = [
+  
+/*
+{
+  date: "",
+  stock: "",
+  buyPrice: null,
+  shares: null,
+  sellPrice: null,
+  desc: "",
+  reason: "",
+  ratings: {
+    valuation: "",
+    growth: "",
+    profitability: "",
+    momentum: "",
+    epsRev: ""
+  }
+},
+*/
+
+
+  {
+    date: "2026/07/14",
+    stock: "BAC",
+    buyPrice: 59.2,
+    shares: 33,
+    sellPrice: 60.4,
+    desc: "Strong Buy.",
+    reason: "嘗試看看這個波動率很低 大概3%的銀行股。",
+    ratings: {
+      valuation: "D+",
+      growth: "B+",
+      profitability: "A+",
+      momentum: "B",
+      epsRev: "B+"
+    }
+  },
+
+  {
+    date: "2026/07/09",
+    stock: "DAL",
+    buyPrice: 88.88,
+    shares: 25,
+    sellPrice: 86.5,
+    desc: "Buy.",
+    reason: "其他評級皆為 A，財報隔日沖觀察標的。",
+    ratings: {
+      valuation: "C+",
+      growth: "B",
+      profitability: "A+",
+      momentum: "A",
+      epsRev: "C+"
+    }
+  },
+
+  {
+    date: "2026/07/07",
+    stock: "PENG",
+    buyPrice: 61.916,
+    shares: 195,
+    sellPrice: 72.566,
+    desc: "Strong Buy.",
+    reason: "PENG 碰到了記憶體板塊的好處，其他指標參數皆優良。",
+    ratings: {
+      valuation: "C",
+      growth: "C",
+      profitability: "C+",
+      momentum: "A+",
+      epsRev: "B-"
+    }
+  },
+
+];
