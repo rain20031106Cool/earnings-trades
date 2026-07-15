64b47db10afe2bcecdbe9eed4fe5b797954ba8a2
rain20031106Cool <rain20031106@gmail.com>
2026-07-16 01:20:55 +0800

    Update trades-data.js

--- trades-data.js
+++ trades-data.js
@@ -18,7 +18,39 @@ const trades = [
   }
 },
 */
+  {
+    date: "2026/07/16",
+    stock: "PNC",
+    buyPrice: 256,
+    shares: 6,
+    sellPrice: 254.1,
+    desc: "Strong Buy",
+    reason: "有個問題是波動率極低，我想這隻不買也罷？",
+    ratings: {
+      valuation: "C-",
+      growth: "C+",
+      profitability: "A",
+      momentum: "B+",
+      epsRev: "B"
+    }
+  },
 
+  {
+    date: "2026/07/14",
+    stock: "ASMU",
+    buyPrice: 33.3,
+    shares: 33,
+    sellPrice: 33,
+    desc: "Hold",
+    reason: "ASML我認為是一間不錯的公司，但評級普普通通，所以買的不多，參數除了Valuation都長得不錯，我想因為這樣才被評為hold。",
+    ratings: {
+      valuation: "F",
+      growth: "B+",
+      profitability: "A+",
+      momentum: "B+",
+      epsRev: "B"
+    }
+  },
 
   {
     date: "2026/07/13",
