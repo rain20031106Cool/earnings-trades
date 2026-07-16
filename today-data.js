6abebd5db9428408f58fd0b30b5e293474897f61
rain20031106Cool <rain20031106@gmail.com>
2026-07-16 23:39:06 +0800

    Update today-data.js

--- today-data.js
+++ today-data.js
@@ -1,49 +1,33 @@
 const todayStocks = [
   {
-    date: "2026/07/15",
-    stock: "UAL",
-    buyPrice: 122,
-    shares: 14,
-    desc: "Buy",
-    reason: "雖然我覺得他很綁定DAL的財報關係，但我覺得他的成長性滿大的。",
+    date: "2026/07/16",
+    stock: "VIST",
+    buyPrice: 64.5,
+    shares: 373,
+    desc: "Strong Buy。",
+    reason: "EPS表現得特別好，近期又是能源股很好的標的。",
     ratings: {
-      valuation: "A-",
-      growth: "C-",
-      profitability: "A+",
-      momentum: "B+",
-      epsRev: "B-"
-    }
-  },
-
-  {
-    date: "2026/07/15",
-    stock: "UNHG",
-    buyPrice: 23.5,
-    shares: 54,
-    desc: "HOLD",
-    reason: "UNH雖然整體的估值跟成長性不佳，但我覺得EPS和momentum、profitability看起來不錯就可以表現好。",
-    ratings: {
-      valuation: "D-",
-      growth: "F",
-      profitability: "A+",
-      momentum: "B+",
-      epsRev: "A-"
+      valuation: "B-",
+      growth: "B+",
+      profitability: "D+",
+      momentum: "A-",
+      epsRev: "A+"
     }
   },
 
     {
-    date: "2026/07/15",
-    stock: "TSMX",
-    buyPrice: 80,
-    shares: 150,
+    date: "2026/07/16",
+    stock: "ISUL",
+    buyPrice: 17,
+    shares: 60,
     desc: "HOLD。",
-    reason: "TSM這理論上是買18股(1500美)，但我覺得評級是valuation把它拉垮，實際上應該能表現得不錯。",
+    reason: "ISRG在未來非常有前景，但是目前表現得不好，我認為法說會的時候會說到未來的獨佔性而股價上升。",
     ratings: {
       valuation: "D-",
-      growth: "A-",
+      growth: "D+",
       profitability: "A+",
-      momentum: "B",
-      epsRev: "A+"
+      momentum: "D+",
+      epsRev: "A-"
     }
   }
   /*
