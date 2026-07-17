41ee03a4b467a64de334342560745d8a1956d8d6
rain20031106Cool <rain20031106@gmail.com>
2026-07-17 22:23:16 +0800

    Update today-data.js

--- today-data.js
+++ today-data.js
@@ -1,35 +1,5 @@
 const todayStocks = [
-  {
-    date: "2026/07/16",
-    stock: "VIST",
-    buyPrice: 64.5,
-    shares: 373,
-    desc: "Strong Buy。",
-    reason: "EPS表現得特別好，近期又是能源股很好的標的。",
-    ratings: {
-      valuation: "B-",
-      growth: "B+",
-      profitability: "D+",
-      momentum: "A-",
-      epsRev: "A+"
-    }
-  },
 
-    {
-    date: "2026/07/16",
-    stock: "ISUL",
-    buyPrice: 17,
-    shares: 60,
-    desc: "HOLD。",
-    reason: "ISRG在未來非常有前景，但是目前表現得不好，我認為法說會的時候會說到未來的獨佔性而股價上升。",
-    ratings: {
-      valuation: "D-",
-      growth: "D+",
-      profitability: "A+",
-      momentum: "D+",
-      epsRev: "A-"
-    }
-  }
   /*
   {
     date: "",
