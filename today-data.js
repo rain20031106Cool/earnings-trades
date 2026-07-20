caa936a33b1455c3b62a75861fc4e00dc0153d5a
rain20031106Cool <rain20031106@gmail.com>
2026-07-20 22:39:58 +0800

    Update today-data.js

--- today-data.js
+++ today-data.js
@@ -1,6 +1,39 @@
 const todayStocks = [
+  {
+    date: "2026/07/20",
+    stock: "VICR",
+    buyPrice: 240,
+    shares: 61,
+    desc: "Strong Buy",
+    reason: "半導體數一數二強的股票，我認為會暴漲。",
+    ratings: {
+      valuation: "D",
+      growth: "A+",
+      profitability: "B-",
+      momentum: "A+",
+      epsRev: "A-"
+    }
+  },
+
+    {
+    date: "2026/07/20",
+    stock: "GM",
+    buyPrice: 159,
+    shares: 76.4,
+    desc: "Strong Buy",
+    reason: "通用汽車屬於穩定的股票，漲幅不大但我依策略進行，不然其實會想要All in VICR。",
+    ratings: {
+      valuation: "B-",
+      growth: "B+",
+      profitability: "A+",
+      momentum: "B",
+      epsRev: "B+"
+    }
+  }
+
 
   /*
+
   {
     date: "",
     stock: "股票代號",
@@ -16,5 +49,6 @@ const todayStocks = [
       epsRev: ""
     }
   }
+
   */
 ];
