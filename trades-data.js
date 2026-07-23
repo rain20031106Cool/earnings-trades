f673e085fa670b8a9ab967dd5c42126cdaffe312
rain20031106Cool <rain20031106@gmail.com>
2026-07-24 03:32:49 +0800

    Update trades-data.js

--- trades-data.js
+++ trades-data.js
@@ -19,6 +19,74 @@ const trades = [
 },
 */
   {
+    date: "2026/07/22",
+    stock: "RTXG",
+    buyPrice: 23.5,
+    shares: 256,
+    sellPrice: 27.04,
+    desc: "Strong Buy",
+    reason: "雷神科技是老牌國防股票，雖然評級幾乎都是HOLDING，但獲利能力有顯著的表現，可惜估值比較高。",
+    ratings: {
+      valuation: "D",
+      growth: "C-",
+      profitability: "A+",
+      momentum: "B-",
+      epsRev: "B"
+    }
+  },
+
+  {
+    date: "2026/07/22",
+    stock: "AALG",
+    buyPrice: 14.1,
+    shares: 426,
+    sellPrice: 11.57,
+    desc: "Strong Buy",
+    reason: "評級全對，相當值得購買！",
+    ratings: {
+      valuation: "A+",
+      growth: "A+",
+      profitability: "A-",
+      momentum: "B+",
+      epsRev: "B-"
+    }
+  },
+
+  {
+    date: "2026/07/22",
+    stock: "STM",
+    buyPrice: 66.17,
+    shares: 181,
+    sellPrice: 55.21,
+    desc: "Strong Buy",
+    reason: "推特白毛股神推薦過的股票，評級不錯，沾上半導體，我覺得有上漲空間。",
+    ratings: {
+      valuation: "D",
+      growth: "A",
+      profitability: "C",
+      momentum: "A",
+      epsRev: "C"
+    }
+  },
+
+    {
+    date: "2026/07/22",
+    stock: "LNOK",
+    buyPrice: 38.17,
+    shares: 40,
+    sellPrice: 34.49,
+    desc: "Hold",
+    reason: "推特白毛股神推薦過的股票，沾上矽光子，碰到點位，評級還可以。",
+    ratings: {
+      valuation: "D-",
+      growth: "D",
+      profitability: "B",
+      momentum: "A-",
+      epsRev: "B-"
+    }
+  },
+
+  {
     date: "2026/07/20",
     stock: "VICR",
     buyPrice: 240,
