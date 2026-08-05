52784447d96c7519b32ade2d08fbb3cbf2838ab2
rain20031106Cool <rain20031106@gmail.com>
2026-08-06 00:32:28 +0800

    Update today-data.js

--- today-data.js
+++ today-data.js
@@ -2,67 +2,21 @@ const todayStocks = [
 
   {
     date: "2026/08/05",
-    stock: "AMDL",
-    buyPrice: 62.5,
-    shares: 180,
+    stock: "SNDU",
+    buyPrice: 19.77,
+    shares: 3780,
     desc: "Strong Buy",
-    reason: "AMD的價格我想還能噴到600，或許在2030年會有800的價值。",
+    reason: "首先呢，縱觀所有當今的記憶體股票，上次尾聲的美光結了15%，這次財報的PENG漲了20%，還有我近期有做一隻STX他也有一段漲幅其中又掩蓋了當天大盤的跌幅。記憶體類股票的價值在這次財報季當中完完全全的呈現了上漲的趨勢。還有一件事情就是閃迪這支股票已經回撤30%，是一個非常好的訊號。如果今天做的結果是暴跌，那麼可以轉為長期投資，也就是說，短線交易可以先停止，因為閃迪這支股票的價格已經太誘人了。但如果已經獲利，那我會出場，繼續做財報短線的沖賣。也就是說，不管漲跌，我都會選擇買這支股票。所以這次才下了重本。",
     ratings: {
-      valuation: "C",
-      growth: "A",
-      profitability: "A+",
-      momentum: "A",
-      epsRev: "B"
-    }
-  },
-
-    {
-    date: "2026/08/04",
-    stock: "APPS",
-    buyPrice: 9.7,
-    shares: 1111,
-    desc: "Strong Buy",
-    reason: "單純參數對了，我還沒有了解這間公司，我找時間了解一下。",
-    ratings: {
-      valuation: "B",
-      growth: "B+",
-      profitability: "C-",
-      momentum: "A",
-      epsRev: "A-"
-    }
-  },
-
-    {
-    date: "2026/08/04",
-    stock: "LSCC",
-    buyPrice: 138,
-    shares: 82,
-    desc: "Strong Buy",
-    reason: "單純參數對了，我還沒有了解這間公司，我找時間了解一下。",
-    ratings: {
-      valuation: "D",
-      growth: "A-",
-      profitability: "B",
-      momentum: "A-",
-      epsRev: "A-"
-    }
-  },
-
-    {
-    date: "2026/08/04",
-    stock: "LABX",
-    buyPrice: 17.5,
-    shares: 610,
-    desc: "Strong Buy",
-    reason: "單純參數對了，我還沒有了解這間公司，我找時間了解一下。",
-    ratings: {
-      valuation: "D",
-      growth: "A",
-      profitability: "A-",
-      momentum: "A",
-      epsRev: "B"
+      valuation: "A",
+      growth: "A+",
+      profitability: "A",
+      momentum: "A+",
+      epsRev: "B+"
     }
   }
+
+
   /*
 
   {
